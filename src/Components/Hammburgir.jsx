6a9a68ba7eccc2be5f0c937aee5f49266ea617eb
Marmik Vyas <161038103+marmikvyass@users.noreply.github.com>
2025-11-25 22:38:55 +0530

    Refactor Hammburgir component to use props

--- src/Components/Hammburgir.jsx
+++ src/Components/Hammburgir.jsx
@@ -1,43 +1,34 @@
-import React from 'react'
-import { useState } from 'react'
-import Hamburger from 'hamburger-react'
-import './Hammburgir.css'
+import React from "react";
+import Hamburger from "hamburger-react";
 
-function Hammburgir() {
-    const [open,setOpen] = useState(false)
-  return <>
-  <div className='hammer'>
+function Hammburgir({ toggled, toggle }) {
+  return (
+    <div className="relative">
+      {/* Hamburger Button */}
+      <div className="sm:hidden cursor-pointer">
+        <Hamburger toggled={toggled} toggle={toggle} />
+      </div>
 
-  
-        <Hamburger
-        toggled={open}
-        toggle={setOpen}
-        />
-        {open && 
-
-        <div>
-            <div className="hamlist">
-                <a href="">
-                    Home
-                </a>
-                                
-                <a href="#container2">
-                    Programs
-                </a>
-                                
-                <a href="#container3">
-                    Plans
-                </a>
-                                
-                <a href="#container4">
-                    Testimonials    
-                </a>
-            </div>
-                            
-        </div>}
+      {/* Dropdown Menu */}
+      {toggled && (
+        <div className="absolute right-0 mt-2 w-48 bg-gray-900 text-white rounded-lg shadow-lg flex flex-col gap-2 p-4 z-50">
+          <a href="#" className="hover:bg-gray-800 px-2 py-1 rounded transition">
+            Home
+          </a>
+          <a href="#container2" className="hover:bg-gray-800 px-2 py-1 rounded transition">
+            Programs
+          </a>
+          <a href="#container3" className="hover:bg-gray-800 px-2 py-1 rounded transition">
+            Plans
+          </a>
+          <a href="#container4" className="hover:bg-gray-800 px-2 py-1 rounded transition">
+            Testimonials
+          </a>
+        </div>
+      )}
     </div>
-  </>
-    
+  );
 }
 
-export default Hammburgir
+export default Hammburgir;
+
