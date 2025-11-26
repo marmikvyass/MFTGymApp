8acbd4e4cd4fbc1ceace8b6bf6ec1afe29097207
Marmik Vyas <161038103+marmikvyass@users.noreply.github.com>
2025-11-26 20:46:46 +0530

    Refactor Strength component layout and styling

--- src/Components/pages/Strength.jsx
+++ src/Components/pages/Strength.jsx
@@ -1,59 +1,79 @@
-import React from 'react'
-import './Strength.css'
+import React from "react";
 
 function Strength() {
   return (
     <>
-    <div className="container1">
-      <h1 className='titleHeader'>
-        Strength Building Workout
-      </h1>
-      <p className='titleDesc'>
-          Increasing strength is no easy feat, but ourstrength-building workout plans can help you achieve your goals.
-      </p>
-      <div className="cards">
-          <div className="cardses">
-            <img className='bicepimg' src="https://cdn.muscleandstrength.com/sites/default/files/styles/800x500/public/power_muscle_burn_5_day_-_1200x630.jpg?itok=dQ3pkPgF" />
-            <h3 className='cardTitle'>Bicep</h3>
-            <p className='cardText'>Want Big Biceps ?</p>
-            <a  href="/Biceps"><button className='cardBtn' >Train Now</button></a>
-          </div>
-          <div className="cardses">
-            <img className='bicepimg' src="https://cdn.muscleandstrength.com/sites/default/files/field/feature-image/workout/sweatshirt-swole-thumb.jpg" />
-            <h3 className='cardTitle'>Back</h3>
-            <p className='cardText'>Want Wide Back ?</p>
-            <a  href="/Back"><button className='cardBtn' >Train Now</button></a>
-           
-          </div>
-          <div className="cardses">
-            <img className='bicepimg' src="https://cdn.muscleandstrength.com/sites/default/files/field/feature-image/workout/crazy-5-method-feature.jpg" />
-            <h3 className='cardTitle'>Chest</h3>
-            <p className='cardText'>Want Huge Biceps ?</p>
-            <a  href="/Chest"><button className='cardBtn' >Train Now</button></a>
-          </div>
-          <div className="cardses">
-            <img className='bicepimg' src="https://imgs.search.brave.com/Lms4SvBWNIDg8vq2rcG9cKl8r0skoerSeUCwRdE6Yb8/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9jbG9zZXVwLW1h/bi1kb2luZy1leGVy/Y2lzZXMtbXVzY2xl/cy10cmljZXBzLXNp/bXVsYXRvci1tdXNj/bGVzLWFybXMtbGln/YW1lbnRzLWNvbmNl/cHQtd29ya291dC1i/b2R5YnVpbGRlci1n/eW1fNjI5NjY1LTE4/ODcuanBnP3NlbXQ9/YWlzX2h5YnJpZCZ3/PTc0MA" />
-            <h3 className='cardTitle'>Tricep</h3>
-            <p className='cardText'>Want Big Tricep ?</p>
-            <a  href="/Tricep"><button className='cardBtn' >Train Now</button></a>
-          </div>
-          <div className="cardses">
-            <img className='bicepimg' src="https://imgs.search.brave.com/qCGfjJaHKFVLN0SZ-PclwNz7aj9m7r8ItYN4EAEww_o/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAxLzEyLzQ3LzQ3/LzM2MF9GXzExMjQ3/NDc0N19VcXNCTzla/QTFEeDdsQVl6a285/WnJqdXNlc2trTmll/aS5qcGc" />
-            <h3 className='cardTitle'>Shoulder</h3>
-            <p className='cardText'>Want Broad Shoulders ?</p>
-            <a  href="/Shoulders"><button className='cardBtn' >Train Now</button></a>
-          </div>
-          <div className="cardses">
-            <img className='bicepimg' src="https://cdn.muscleandstrength.com/sites/default/files/field/feature-image/workout/pause-reps-for-size-feature.jpg" />
-            <h3 className='cardTitle'>Legs</h3>
-            <p className='cardText'>Want Muscular Legs ?</p>
-            <a  href="/Legs"><button className='cardBtn' >Train Now</button></a>
-          </div>
-      </div>
-    </div>
+      <div className="min-h-screen w-full bg-linear-to-br from-[#003153] to-[#1B1B1B] text-white">
+        <h1 className="text-white text-4xl sm:text-5xl font-extrabold text-center pt-5 mb-1">
+          Strength Building Workout
+        </h1>
 
+        <p className="text-lg font-medium text-center px-4">
+          Increasing strength is no easy feat, but our strength-building workout
+          plans can help you achieve your goals.
+        </p>
+
+        <div className="grid sm:grid-cols-3 grid-cols-1 place-items-center mt-6 gap-5 px-10 ">
+          {[
+            {
+              id  : 1,
+              title: "Bicep",
+              desc: "Want Big Biceps ?",
+              link : "/Biceps"
+            },
+            {
+              id  : 2,
+              title: "Chest",
+              desc: "Want Big Chest ?",
+              link : "/Chest"
+            },
+            {
+              id  : 3,
+              title: "Back",
+              desc: "Want Huge Back ?",
+              link : "/Back"
+            },
+            {
+              id  : 4,
+              title: "Tricep",
+              desc: "Want Big Tricep ?",
+              link : "/Tricep"
+            },
+            {
+              id  : 5,
+              title: "Shoulders",
+              desc: "Want Rounded Shoulders ?",
+              link : "/Shoulders"
+            },
+            {
+              id  : 6,
+              title: "Legs",
+              desc: "Want Strong Legs ?",
+              link : "/Legs"
+            },
+          ].map((workout) => (
+            <div key={workout.id} className="bg-linear-to-br from-[#cdc1ff] to-[#e5d9f2] w-65 h-65 rounded-lg shadow-lg transform transition hover:scale-105 mb-10">
+              <img
+                className="rounded-t-lg h-[60%] w-full object-cover"
+                src="https://cdn.muscleandstrength.com/sites/default/files/styles/800x500/public/power_muscle_burn_5_day_-_1200x630.jpg?itok=dQ3pkPgF"
+              />
+              <h3 className="text-black text-xl font-semibold px-3 mt-1">
+                {workout.title}
+              </h3>
+              <p className="text-black text-md px-3">{workout.desc}</p>
+              <a href={workout.link}>
+                <button className="rounded-md h-6 w-22 border border-black bg-transparent text-black text-sm font-medium mx-3 mt-2 transition hover:scale-110 hover:bg-black hover:text-white cursor-pointer">
+                  Train Now
+                </button>
+              </a>
+            </div>
+          ))
+          }
+
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Strength
+export default Strength;
